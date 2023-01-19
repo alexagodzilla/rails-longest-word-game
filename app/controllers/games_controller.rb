3da1e41a8b05c2cc92458245566b4db97c91efae
Alessandro Agozzino <alexagozzino@gmail.com>
2023-01-19 21:01:13 +0000

    validation and API check add

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,7 @@
+require 'set'
+require "open-uri"
+require "json"
+
 class GamesController < ApplicationController
   def new
     array = []
@@ -9,6 +13,18 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @word = params[:question]
+    @new_letters = params[:letters].chars
+    @input = params[:question].chars
+    @result = (@input.to_set).subset?(@new_letters.to_set)
+    if @result
+      @result = "You got the word"
+      url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+      user_serialized = URI.open(url).read
+      user = JSON.parse(user_serialized)
+      @answer = user["found"]
+    else
+      @result = "You didin't get the word"
+    end
   end
 end
